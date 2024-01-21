8d1e1022e68841bec0a073861ccf8e116c108dbd
AlexanderDerevliov <alex.feelred@gmail.com>
2024-01-21 18:49:38 +0100

    add: carbon

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Train;
-
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -25,6 +25,13 @@ class TrainsTableSeeder extends Seeder
         $train->company = $faker->randomElement('$companies');
         $train->departure_station = $faker->randomElement('departure_stations');
         $train->arrival_station = $faker->randomElement('arival_stations');
+
+        $train->departure_time = $faker->dateTimeBetween('-1 day', '+1 day');
+
+        $carbon_departure = new Carbon($train->departure_time);
+        $carbon_arrival = $carbon_departure->addHours($faker->numberBetween(1,24))->format('Y-m-d H:i:s');
+
+
 
         
     }
